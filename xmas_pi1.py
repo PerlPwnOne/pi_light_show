a5286cb16836217311e2ea07a032e61b66c2f209
dawg31wh <hansenwdawg@comcast.net>
2017-12-04 23:10:49 -0500

    version 2

--- xmas_pi1.py
+++ xmas_pi1.py
@@ -33,7 +33,19 @@ SleepTimeL = 2.5
 
 try:
     while True:
-        i = random.sample([7, 8, 25, 24, 23, 2, 3, 4], 5)
+	  next_step = seq_data[step].split(",");
+	  next_step[1] = next_step[1].rstrip()
+	  cur_time = int(round(time.time()*1000)) - start_time
+
+# time to run the command
+    if int(next_step[0]) <= cur_time:
+
+    print next_step
+# if the command is Relay 1-16 
+    if next_step[1] >= "1" and next_step[1] <= "16":
+
+# change the pin state
+        i = random.sample([7, 8, 25, 24, 23, 2, 3, 4, 17, 27, 22, 10, 9, 14, 15, 18], 5)
         GPIO.output(i, GPIO.LOW)
         time.sleep(SleepTimeL);
         GPIO.output(i, GPIO.HIGH)
